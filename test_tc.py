0d467fc5176bceba38aa3a6b9e32b17ea721e754
bohdan <dovbysh.bohdan@gmail.com>
2023-08-10 21:01:29 +0300

    Update test_tc.py

--- test_tc.py
+++ test_tc.py
@@ -2,7 +2,7 @@ def test_01():
     assert 1 == 1, 'Message'
 
 def test_02():
-    assert 1 == 1, 'Message'
+    assert 1 == 2, 'Message'
 
 def test_03():
-    assert 1 == 1, 'Message'
+    assert 1 == 3, 'Message'
